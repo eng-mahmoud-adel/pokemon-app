90770585d4a4cb946cbbb21b4d735beed6543e8a
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2025-11-14 23:18:41 +0200

    add skeleton to PokemonListPage while fetching data

--- src/pages/PokemonListPage.tsx
+++ src/pages/PokemonListPage.tsx
@@ -1,11 +1,20 @@
 import PokemonList from "@/components/PokemonList";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { SkeletonCard } from "@/components/SkeletonCard";
 
 const PokemonListPage = () => {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="grid grid-cols-4 gap-4">
+            {Array.from({ length: 20 }, (_, index) => (
+              <SkeletonCard key={index} />
+            ))}
+          </div>
+        }
+      >
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
           <PokemonList />
         </ErrorBoundary>
