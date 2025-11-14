fc54dea52f517c69f3e63cda510e95d505d4e1f1
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2025-11-14 20:47:19 +0200

    add intial values to queryClient instance

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,15 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchOnMount: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
